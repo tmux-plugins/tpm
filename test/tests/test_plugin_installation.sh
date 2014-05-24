45cd3d40fa6b959ba10a1292b183c1eb21865fcf
Bruno Sutic <bruno.sutic@gmail.com>
2014-05-24 15:38:41 +0200

    First version of tests

---
+++ test/tests/test_plugin_installation.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $CURRENT_DIR/helpers.sh
+
+FAIL=false
+
+test_plugin_installation() {
+	set_tmux_conf_helper <<- HERE
+	set -g @tpm_plugins "bruno-/tmux_example_plugin"
+	run-shell '~/tmux_plugin_manager/tpm'
+	HERE
+
+  # opens tmux and test it with `expect`
+  $CURRENT_DIR/expect_successful_plugin_download ||
+      (echo "Tmux plugin installation fails" >&2; FAIL=true)
+
+  # check plugin dir exists after download
+  check_dir_exists_helper "/root/.tmux/plugins/tmux_example_plugin/" ||
+      (echo "Plugin download fails" >&2; FAIL=true)
+
+	teardown_helper
+}
+
+main() {
+    test_plugin_installation
+    exit_value_helper
+}
+main
---
+++ test/tests/test_plugin_sourcing.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $CURRENT_DIR/helpers.sh
+
+FAIL=false
+
+check_binding_defined() {
+    local binding=$1
+    tmux list-keys | grep -q "$binding"
+}
+
+test_plugin_sourcing() {
+	set_tmux_conf_helper <<- HERE
+	set -g @tpm_plugins "doesnt_matter/tmux_test_plugin"
+	# change this
+	run-shell '~/tmux_plugin_manager/tpm'
+	HERE
+
+	create_test_plugin_helper <<- HERE
+	tmux bind-key R run-shell foo_command
+	HERE
+
+  tmux new-session -d  # tmux starts detached
+  check_binding_defined 'R run-shell foo_command' ||
+      (echo "Plugin sourcing fails" >&2; FAIL=true)
+
+	teardown_helper
+}
+
+main() {
+    test_plugin_sourcing
+    exit_value_helper
+}
+main

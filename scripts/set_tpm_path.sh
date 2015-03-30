5bd15b32f38fd3da14b21cbd19bd86142aa13c85
Bruno Sutic <bruno.sutic@gmail.com>
2015-03-30 13:50:04 +0200

    Small refactor

--- scripts/set_tpm_path.sh
+++ scripts/set_tpm_path.sh
@@ -10,13 +10,9 @@ set_default_tpm_path() {
 	tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "$default_tpm_path"
 }
 
-ensure_tpm_path() {
+main() {
 	if ! tpm_path_set; then
 		set_default_tpm_path
 	fi
-}
-
-main() {
-	ensure_tpm_path
 }
 main

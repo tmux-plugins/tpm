2d5ecdbe4a958233050c88a4238a66ea8b21dcec
Javier Lopez <m@javier.io>
2015-05-26 22:46:15 -0500

    add removal procedure

---
+++ scripts/clean_plugins.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/shared_functions.sh"
+
+clean_plugins() {
+	local plugins plugin plugin_directory
+	plugins="$(shared_get_tpm_plugins_list)"
+
+	for plugin_directory in "$SHARED_TPM_PATH"/*; do
+		[ -d "${plugin_directory}" ] || continue
+		plugin="$(shared_plugin_name "${plugin_directory}")"
+		case "${plugins}" in
+			*"${plugin}"*) : ;;
+			*)
+			[ "${plugin}" = "tpm" ] && continue
+			echo_message "Removing \"$plugin\""
+			rm -rf "${plugin_directory}"
+			[ -d "${plugin_directory}" ] &&
+			echo_message "  \"$plugin\" clean fail" ||
+			echo_message "  \"$plugin\" clean success"
+			;;
+		esac
+	done
+}
+
+ensure_tpm_path_exists() {
+	mkdir -p "$SHARED_TPM_PATH"
+}
+
+reload_tmux_environment() {
+	tmux source-file ~/.tmux.conf >/dev/null 2>&1
+}
+
+main() {
+	reload_tmux_environment
+	shared_set_tpm_path_constant
+	ensure_tpm_path_exists
+	clean_plugins
+	reload_tmux_environment
+	end_message
+}
+main
--- scripts/variables.sh
+++ scripts/variables.sh
@@ -3,3 +3,6 @@ default_install_key="I"
 
 update_key_option="@tpm-update"
 default_update_key="U"
+
+clean_key_option="@tpm-clean"
+default_clean_key="Y"

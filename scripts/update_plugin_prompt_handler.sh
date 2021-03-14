233798809800f8261bbf01b1decbe61aec5a5b9b
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2021-03-14 22:25:45 +0700

    Fix tmux_utils.sh redefining CURRENT_DIR for scripts/update_plugin_prompt_handler.sh

--- scripts/update_plugin_prompt_handler.sh
+++ scripts/update_plugin_prompt_handler.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTS_DIR="$CURRENT_DIR"
 HELPERS_DIR="$CURRENT_DIR/helpers"
 
 if [ $# -eq 0 ]; then
@@ -8,10 +9,11 @@ if [ $# -eq 0 ]; then
 fi
 
 source "$HELPERS_DIR/tmux_echo_functions.sh"
+# NOTE: This line redifines CURRENT_DIR
 source "$HELPERS_DIR/tmux_utils.sh"
 
 main() {
-	"$CURRENT_DIR/update_plugin.sh" --tmux-echo "$*"
+	"$SCRIPTS_DIR/update_plugin.sh" --tmux-echo "$*"
 	reload_tmux_environment
 	end_message
 }

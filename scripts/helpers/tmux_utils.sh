0a19b28b4e1c1ad87932d633516b9c963e9f58bd
Bruno Sutic <code@brunosutic.com>
2021-03-15 11:41:53 +0100

    Remove `CURRENT_DIR` variable from a helper script
    Fixes #200

--- scripts/helpers/tmux_utils.sh
+++ scripts/helpers/tmux_utils.sh
@@ -1,6 +1,3 @@
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$CURRENT_DIR/plugin_functions.sh"
-
 reload_tmux_environment() {
 	tmux source-file $(_get_user_tmux_conf) >/dev/null 2>&1
 }

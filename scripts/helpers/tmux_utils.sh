2afeff1529ec85d0c5ced5ece3714c2220b646a5
Bruno Sutic <code@brunosutic.com>
2021-03-15 11:50:50 +0100

    Add 'HELPERS_DIR' variable to tmux_utils.sh

--- scripts/helpers/tmux_utils.sh
+++ scripts/helpers/tmux_utils.sh
@@ -1,3 +1,6 @@
+HELPERS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$HELPERS_DIR/plugin_functions.sh"
+
 reload_tmux_environment() {
 	tmux source-file $(_get_user_tmux_conf) >/dev/null 2>&1
 }

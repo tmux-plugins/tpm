e05d6c6589b5b01d88526018c6baac1728e82ec3
Nexn <72174296+Itsnexn@users.noreply.github.com>
2021-10-24 15:35:30 +0330

    Quick fix for #213 :hammer:
    somehow uppercase letters in the plugin name make the server freeze and after the restart, it shows the "server exited unexpectedly" message!

--- scripts/helpers/plugin_functions.sh
+++ scripts/helpers/plugin_functions.sh
@@ -74,7 +74,7 @@ tpm_plugins_list_helper() {
 
 	# read set -g @plugin "tmux-plugins/tmux-example-plugin" entries
 	_tmux_conf_contents "full" |
-		awk '/^[ \t]*set(-option)? +-g +@plugin/ { gsub(/'\''/,""); gsub(/'\"'/,""); print $4 }'
+		awk '/^[ \t]*set(-option)? +-g +@plugin/ { gsub(/'\''/,""); gsub(/'\"'/,""); print tolower($4) }'
 }
 
 # Allowed plugin name formats:
